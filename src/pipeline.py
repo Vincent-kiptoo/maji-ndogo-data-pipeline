e5435079eb82d4711f4a76b2e041d613e9e9c9ac
Vincent Kiptoo <vincentkiptoo2232@gmail.com>
2026-07-11 14:24:36 +0300

    Merged both field_df and weather_df to get a single unified dataset for further analysis

---
+++ src/pipeline.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from src.field_data_processor import FieldDataProcessor
+from src.weather_data_processor import WeatherDataProcessor
+from src.config import config_params
+
+
+def create_final_dataset():
+
+    field_df = FieldDataProcessor(config_params).process()
+
+    weather_df = WeatherDataProcessor(config_params).process()
+    if field_df is None:
+        raise ValueError("Field_df is an empty DataFrame")
+    if weather_df is None:
+        raise ValueError("weather_df is an empty DataFrame")
+    final_df = field_df.merge(
+        weather_df,
+        on="Weather_station_ID",
+        how="left"
+    )
+
+    final_df = final_df.rename(
+        columns={
+            "Rainfall_x": "Field_Rainfall",
+            "Rainfall_y": "Station_Rainfall",
+            "Pollution_level_x": "Field_Pollution_Level",
+            "Pollution_level_y": "Station_Pollution_Level"
+        }
+    )
+
+    return final_df
